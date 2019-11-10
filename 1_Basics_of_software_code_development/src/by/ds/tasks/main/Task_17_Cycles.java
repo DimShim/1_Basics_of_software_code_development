f441e1af743c89dd543cc2830b298c3856107f0f
Dima Shimanel <en_di@mail.ru>
2019-11-10 17:25:39 +0200

    Dec -symbol

--- 1_Basics_of_software_code_development/src/by/ds/tasks/main/Task_17_Cycles.java
+++ 1_Basics_of_software_code_development/src/by/ds/tasks/main/Task_17_Cycles.java
@@ -8,7 +8,7 @@ public class Task_17_Cycles {
 	public static void main(String[] args){
 		
       for(int i =0; i<256; i++){
-    	  System.out.println( i + ". " + (char)i);
+    	  System.out.println( "DEC " + i + " - " + (char)i);
         }
     }
 }
