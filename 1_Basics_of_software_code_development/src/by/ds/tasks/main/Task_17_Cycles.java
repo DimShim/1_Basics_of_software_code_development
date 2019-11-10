d2e0fa2846d5abf01a2de69e19e6076b41bfe1a1
Dima Shimanel <en_di@mail.ru>
2019-11-10 17:22:34 +0200

    DEC - symbol

--- 1_Basics_of_software_code_development/src/by/ds/tasks/main/Task_17_Cycles.java
+++ 1_Basics_of_software_code_development/src/by/ds/tasks/main/Task_17_Cycles.java
@@ -5,5 +5,10 @@ package by.ds.tasks.main;
  */
 
 public class Task_17_Cycles {
-
+	public static void main(String[] args){
+		
+      for(int i =0; i<256; i++){
+    	  System.out.println( i + ". " + (char)i);
+        }
+    }
 }
