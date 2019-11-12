0ae34a30d55a5b3bce684dc33c80a17d98bad461
Dima Shimanel <en_di@mail.ru>
2019-11-12 20:51:05 +0200

    Numbers

--- 1_Basics_of_software_code_development/src/by/ds/tasks/main/Task_19_Cycles.java
+++ 1_Basics_of_software_code_development/src/by/ds/tasks/main/Task_19_Cycles.java
@@ -18,19 +18,19 @@ public class Task_19_Cycles {
 		int s = b;
 		int z = 0;
 		int v = 0;
-		int  pq=0, pw=0,pe=0,pr=0,pt=0,py=0,pu=0,pi=0,po=0,pp=0;
+		byte  pq=0, pw=0,pe=0,pr=0,pt=0,py=0,pu=0,pi=0,po=0,pp=0;
 		
 		while(a != 0) {		//перебор первого числа
 			int c = a % 10;
 			
 			while (b != 0) {	// перебор второго числа
 				int x = b % 10;
-				if(x == c) 
+				if(x == c) { 		// выводим число только один раз
 					/////////////////
-					if(x == 0) { 		// выводим число только один раз
+					if(x == 0) { 		
 						pq++;
 						if(pq==1) {
-						int q=0;
+							byte q=0;
 						System.out.print(" "+ q);
 						}
 					} 
@@ -38,7 +38,7 @@ public class Task_19_Cycles {
 						if(x == 1){
 							pw++;
 							if(pw==1) {
-							int w=1;
+								byte w=1;
 							System.out.print(" "+ w);
 							}
 						} 
@@ -46,7 +46,7 @@ public class Task_19_Cycles {
 							if(x == 2){
 								pe++;
 								if(pe==1) {
-								int e=2;
+									byte e=2;
 								System.out.print(" "+ e);
 								}
 							} 
@@ -54,7 +54,7 @@ public class Task_19_Cycles {
 								if(x == 3){
 									pr++;
 									if(pr==1) {
-									int r=3;
+										byte r=3;
 									System.out.print(" "+ r);
 									}
 								} 
@@ -62,7 +62,7 @@ public class Task_19_Cycles {
 									if(x == 4){
 										pt++;
 										if(pt==1) {
-										int t=4;
+											byte t=4;
 										System.out.print(" "+ t);
 										}
 									} 
@@ -70,7 +70,7 @@ public class Task_19_Cycles {
 										if(x == 5){
 											py++;
 											if(py==1) {
-											int y=5;
+												byte y=5;
 											System.out.print(" "+ y);
 											}
 										} 
@@ -78,7 +78,7 @@ public class Task_19_Cycles {
 											if(x == 6){
 												pu++;
 												if(pu==1) {
-												int u=6;
+													byte u=6;
 												System.out.print(" "+ u);
 												}
 											} 
@@ -86,7 +86,7 @@ public class Task_19_Cycles {
 												if(x == 7){
 													pi++;
 													if(pi==1) {
-													int i=7;
+														byte i=7;
 													System.out.print(" "+ i);
 													}
 												} 
@@ -94,7 +94,7 @@ public class Task_19_Cycles {
 													if(x == 8){
 														po++;
 														if(po==1) {
-														int o=8;
+															byte o=8;
 														System.out.print(" "+ o);
 														}
 													} 
@@ -102,10 +102,11 @@ public class Task_19_Cycles {
 														if(x == 9){
 															pp++;
 															if(pp==1) {
-															int p=9;
+																byte p=9;
 															System.out.print(" "+ p);
 															}
 														} 
+				}
 				// проверяем повторяющиеся подряд цифры для второго числа
 				do {
 					b = (b-x)/10;
